13cce29869c9e8078c12856a759ae6886b169146
saniyaPC <saniya@purecodesoftware.com>
2024-02-23 18:42:29 +0530

    Input group page

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import { Provider } from "react-redux";
 import Store from "./services/Store";
 import TopBar from "./components/TopBar";
 import MenuBar from "./components/MenuBar";
+import FormInputGroups from "pages/FormInputGroups";
 
 const App = () => {
   return (
     <Provider store={Store}>
       <TopBar/>
       <MenuBar/>
+      <FormInputGroups/>
     </Provider>
   );
 };
