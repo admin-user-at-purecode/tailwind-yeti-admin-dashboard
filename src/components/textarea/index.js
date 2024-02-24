fb2beced55acf6e77f40bda7c465215a68ae3cc6
saniyaPC <saniya@purecodesoftware.com>
2024-02-24 12:33:19 +0530

    all components structured files

---
+++ src/components/textarea/index.js
@@ -0,0 +1,24 @@
+import classNames from "classnames";
+import PropTypes from "prop-types";
+
+const Textarea = (props) => {
+  const { invalid, className, children, ...rest } = props;
+
+  return (
+    <textarea
+      className={classNames(
+        "form-control",
+        { "is-invalid": invalid },
+        className
+      )}
+      {...rest}
+    ></textarea>
+  );
+};
+
+Textarea.propTypes = {
+  invalid: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+export default Textarea;
