1224712f5bb9706c049761bcc74e6953176a435f
saimanikanta17 <sivasatya1798@gmail.com>
2024-02-26 19:40:44 +0530

    added ui tables

--- src/mock_data.js
+++ src/mock_data.js
@@ -1,52 +1,52 @@
 export const MenuDetailUIItems = [
-  { text: "Components", icon: "la la-cubes"},
-  { text: "Input Groups", icon: "la la-stop"},
-  { text: "Layout", icon: "la la-th-large"},
-  { text: "Validations", icon: "la la-check-circle"},
-  { text: "Wizards", icon: "la la-hand-pointer"},
-  { text: "Alerts", icon: "la la-bell"},
-  { text: "Avatars", icon: "la la-user-circle"},
-  { text: "Badges", icon: "la la-certificate"},
-  { text: "Buttons", icon: "la la-play"},
-  { text: "Cards", icon: "la la-layer-group"},
-  { text: "Collapse", icon: "la la-arrow-circle-right"},
-  { text: "Colors", icon: "la la-palette"},
-  { text: "Dropdowns", icon: "la la-arrow-circle-down"},
-  { text: "Modal", icon: "la la-times-circle"},
-  { text: "Popovers & Tooltips", icon: "la la-thumbtack"},
-  { text: "Tabs", icon: "la la-columns"},
-  { text: "Tables", icon: "la la-table"},
-  { text: "Toasts", icon: "la la-bell"},
-  { text: "Carousel", icon: "la la-images"},
-  { text: "Charts", icon: "la la-chart-area"},
-  { text: "Editors", icon: "la la-keyboard"},
-  { text: "FullCalendar", icon: "la la-calendar"},
-  { text: "Sortable", icon: "la la-sort"},
+  { text: "Components", icon: "la la-cubes" },
+  { text: "Input Groups", icon: "la la-stop" },
+  { text: "Layout", icon: "la la-th-large" },
+  { text: "Validations", icon: "la la-check-circle" },
+  { text: "Wizards", icon: "la la-hand-pointer" },
+  { text: "Alerts", icon: "la la-bell" },
+  { text: "Avatars", icon: "la la-user-circle" },
+  { text: "Badges", icon: "la la-certificate" },
+  { text: "Buttons", icon: "la la-play" },
+  { text: "Cards", icon: "la la-layer-group" },
+  { text: "Collapse", icon: "la la-arrow-circle-right" },
+  { text: "Colors", icon: "la la-palette" },
+  { text: "Dropdowns", icon: "la la-arrow-circle-down" },
+  { text: "Modal", icon: "la la-times-circle" },
+  { text: "Popovers & Tooltips", icon: "la la-thumbtack" },
+  { text: "Tabs", icon: "la la-columns" },
+  { text: "Tables", icon: "la la-table" },
+  { text: "Toasts", icon: "la la-bell" },
+  { text: "Carousel", icon: "la la-images" },
+  { text: "Charts", icon: "la la-chart-area" },
+  { text: "Editors", icon: "la la-keyboard" },
+  { text: "FullCalendar", icon: "la la-calendar" },
+  { text: "Sortable", icon: "la la-sort" },
 ];
 
 export const MenuDetailPagesItems = [
-    { text: "Login", icon: "la la-user"},
-    { text: "Forgot Password", icon: "la la-user-lock"},
-    { text: "Register", icon: "la la-user-plus"},
-    { text: "List", icon: "la la-list"},
-    { text: "List - Card Rows", icon: "la la-list"},
-    { text: "List - Card Columns", icon: "la la-list"},
-    { text: "Add Post", icon: "la la-layer-group"},
-    { text: "403 Error", icon: "la la-exclamation-circle"},
-    { text: "404 Error", icon: "la la-exclamation-circle"},
-    { text: "500 Error", icon: "la la-exclamation-circle"},
-    { text: "Under Maintenance", icon: "la la-exclamation-circle"},
-    { text: "Pricing", icon: "la la-dollar"},
-    { text: "FAQs - Layout 1", icon: "la la-question-circle"},
-    { text: "FAQs - Layout 2", icon: "la la-question-circle"},
-    { text: "Invoice", icon: "la la-file-invoice-dollar"},
-  ];
+  { text: "Login", icon: "la la-user" },
+  { text: "Forgot Password", icon: "la la-user-lock" },
+  { text: "Register", icon: "la la-user-plus" },
+  { text: "List", icon: "la la-list" },
+  { text: "List - Card Rows", icon: "la la-list" },
+  { text: "List - Card Columns", icon: "la la-list" },
+  { text: "Add Post", icon: "la la-layer-group" },
+  { text: "403 Error", icon: "la la-exclamation-circle" },
+  { text: "404 Error", icon: "la la-exclamation-circle" },
+  { text: "500 Error", icon: "la la-exclamation-circle" },
+  { text: "Under Maintenance", icon: "la la-exclamation-circle" },
+  { text: "Pricing", icon: "la la-dollar" },
+  { text: "FAQs - Layout 1", icon: "la la-question-circle" },
+  { text: "FAQs - Layout 2", icon: "la la-question-circle" },
+  { text: "Invoice", icon: "la la-file-invoice-dollar" },
+];
 
 export const MenuDetailApplicationsItems = [
-  { text: "Media Library", icon: "la la-image"},
-  { text: "Point Of Sale", icon: "la la-shopping-bag"},
-  { text: "To Do", icon: "la la-check-circle"},
-  { text: "Chat", icon: "la la-comment"},
+  { text: "Media Library", icon: "la la-image" },
+  { text: "Point Of Sale", icon: "la la-shopping-bag" },
+  { text: "To Do", icon: "la la-check-circle" },
+  { text: "Chat", icon: "la la-comment" },
 ];
 
 export const MenuDetailMenuItems = [
@@ -70,13 +70,17 @@ export const topBarData = [
 ];
 
 export const tabLeftData = [
-  {title: "Heading One", text: "Today"},
-  {title: "Heading Two", text: "Yesterday"},
-  {title: "Heading Three", text: "Last Week"},
-]
+  { title: "Heading One", text: "Today" },
+  { title: "Heading Two", text: "Yesterday" },
+  { title: "Heading Three", text: "Last Week" },
+];
 
 export const tabProfileDetails = [
-  {title: "View Profile", iconClass: "la-user-circle" },
-  {title: "Change Password", iconClass: "la-key" },
-  {title: "Logout", iconClass: "la-power-off" },
-]
+  { title: "View Profile", iconClass: "la-user-circle" },
+  { title: "Change Password", iconClass: "la-key" },
+  { title: "Logout", iconClass: "la-power-off" },
+];
+
+export const TableHeadings = ["#", "First", "Last", "Handle"];
+
+export const tableData = [1, 2, 3];
---
+++ src/pages/tables/index.js
@@ -0,0 +1,146 @@
+import Footer from "components/footer";
+
+import Breadcrumb, { BreadcrumbItem } from "components/breadcrumb";
+import { TableHeadings, tableData } from "mock_data";
+
+const ComponentsTables = () => {
+  return (
+    <main className="workspace">
+      <Breadcrumb title="Tables">
+        <BreadcrumbItem link="#no-link">UI</BreadcrumbItem>
+        <BreadcrumbItem link="#no-link">Components</BreadcrumbItem>
+        <BreadcrumbItem>Tables</BreadcrumbItem>
+      </Breadcrumb>
+
+      <div className="grid lg:grid-cols-2 gap-5">
+        <div className="flex flex-col gap-y-5">
+          <div className="card p-5">
+            <h3>Basic</h3>
+            <table className="table w-full mt-3">
+              <thead>
+                <tr>
+                  {TableHeadings.map((heading) => (
+                    <th className="ltr:text-left rtl:text-right uppercase">
+                      {heading}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {tableData.map((val) => (
+                  <tr>
+                    <td>{val}</td>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>@john</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="card p-5">
+            <h3>Bordered</h3>
+            <table className="table table_bordered w-full mt-3">
+              <thead>
+                <tr>
+                  {TableHeadings.map((heading) => (
+                    <th className="ltr:text-left rtl:text-right uppercase">
+                      {heading}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {tableData.map((val) => (
+                  <tr>
+                    <td>{val}</td>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>@john</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="card p-5">
+            <h3>Hoverable</h3>
+            <table className="table table_hoverable w-full mt-3">
+              <thead>
+                <tr>
+                  {TableHeadings.map((heading) => (
+                    <th className="ltr:text-left rtl:text-right uppercase">
+                      {heading}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {tableData.map((val) => (
+                  <tr>
+                    <td>{val}</td>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>@john</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="flex flex-col gap-y-5">
+          <div className="card p-5">
+            <h3>Striped</h3>
+            <table className="table table_striped w-full mt-3">
+              <thead>
+                <tr>
+                  {TableHeadings.map((heading) => (
+                    <th className="ltr:text-left rtl:text-right uppercase">
+                      {heading}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {tableData.map((val) => (
+                  <tr>
+                    <td>{val}</td>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>@john</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="card p-5">
+            <h3>Borderless</h3>
+            <table className="table table_borderless w-full mt-3">
+              <thead>
+                <tr>
+                  {TableHeadings.map((heading) => (
+                    <th className="ltr:text-left rtl:text-right uppercase">
+                      {heading}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {tableData.map((val) => (
+                  <tr>
+                    <td>{val}</td>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>@john</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </main>
+  );
+};
+
+export default ComponentsTables;
