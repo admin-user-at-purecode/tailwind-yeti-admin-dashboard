cd4a7e2b34ea9cfb07955a37bdabd81586167e6b
Harshith-1629 <harshith.bandam@purecodemarketplace.io>
2024-02-26 15:23:43 +0530

    pagesbloglistcardcolumn

--- src/mock_data.js
+++ src/mock_data.js
@@ -1,52 +1,52 @@
 export const MenuDetailUIItems = [
-  { text: "Components", icon: "la la-cubes"},
-  { text: "Input Groups", icon: "la la-stop"},
-  { text: "Layout", icon: "la la-th-large"},
-  { text: "Validations", icon: "la la-check-circle"},
-  { text: "Wizards", icon: "la la-hand-pointer"},
-  { text: "Alerts", icon: "la la-bell"},
-  { text: "Avatars", icon: "la la-user-circle"},
-  { text: "Badges", icon: "la la-certificate"},
-  { text: "Buttons", icon: "la la-play"},
-  { text: "Cards", icon: "la la-layer-group"},
-  { text: "Collapse", icon: "la la-arrow-circle-right"},
-  { text: "Colors", icon: "la la-palette"},
-  { text: "Dropdowns", icon: "la la-arrow-circle-down"},
-  { text: "Modal", icon: "la la-times-circle"},
-  { text: "Popovers & Tooltips", icon: "la la-thumbtack"},
-  { text: "Tabs", icon: "la la-columns"},
-  { text: "Tables", icon: "la la-table"},
-  { text: "Toasts", icon: "la la-bell"},
-  { text: "Carousel", icon: "la la-images"},
-  { text: "Charts", icon: "la la-chart-area"},
-  { text: "Editors", icon: "la la-keyboard"},
-  { text: "FullCalendar", icon: "la la-calendar"},
-  { text: "Sortable", icon: "la la-sort"},
+  { text: "Components", icon: "la la-cubes" },
+  { text: "Input Groups", icon: "la la-stop" },
+  { text: "Layout", icon: "la la-th-large" },
+  { text: "Validations", icon: "la la-check-circle" },
+  { text: "Wizards", icon: "la la-hand-pointer" },
+  { text: "Alerts", icon: "la la-bell" },
+  { text: "Avatars", icon: "la la-user-circle" },
+  { text: "Badges", icon: "la la-certificate" },
+  { text: "Buttons", icon: "la la-play" },
+  { text: "Cards", icon: "la la-layer-group" },
+  { text: "Collapse", icon: "la la-arrow-circle-right" },
+  { text: "Colors", icon: "la la-palette" },
+  { text: "Dropdowns", icon: "la la-arrow-circle-down" },
+  { text: "Modal", icon: "la la-times-circle" },
+  { text: "Popovers & Tooltips", icon: "la la-thumbtack" },
+  { text: "Tabs", icon: "la la-columns" },
+  { text: "Tables", icon: "la la-table" },
+  { text: "Toasts", icon: "la la-bell" },
+  { text: "Carousel", icon: "la la-images" },
+  { text: "Charts", icon: "la la-chart-area" },
+  { text: "Editors", icon: "la la-keyboard" },
+  { text: "FullCalendar", icon: "la la-calendar" },
+  { text: "Sortable", icon: "la la-sort" },
 ];
 
 export const MenuDetailPagesItems = [
-    { text: "Login", icon: "la la-user"},
-    { text: "Forgot Password", icon: "la la-user-lock"},
-    { text: "Register", icon: "la la-user-plus"},
-    { text: "List", icon: "la la-list"},
-    { text: "List - Card Rows", icon: "la la-list"},
-    { text: "List - Card Columns", icon: "la la-list"},
-    { text: "Add Post", icon: "la la-layer-group"},
-    { text: "403 Error", icon: "la la-exclamation-circle"},
-    { text: "404 Error", icon: "la la-exclamation-circle"},
-    { text: "500 Error", icon: "la la-exclamation-circle"},
-    { text: "Under Maintenance", icon: "la la-exclamation-circle"},
-    { text: "Pricing", icon: "la la-dollar"},
-    { text: "FAQs - Layout 1", icon: "la la-question-circle"},
-    { text: "FAQs - Layout 2", icon: "la la-question-circle"},
-    { text: "Invoice", icon: "la la-file-invoice-dollar"},
-  ];
+  { text: "Login", icon: "la la-user" },
+  { text: "Forgot Password", icon: "la la-user-lock" },
+  { text: "Register", icon: "la la-user-plus" },
+  { text: "List", icon: "la la-list" },
+  { text: "List - Card Rows", icon: "la la-list" },
+  { text: "List - Card Columns", icon: "la la-list" },
+  { text: "Add Post", icon: "la la-layer-group" },
+  { text: "403 Error", icon: "la la-exclamation-circle" },
+  { text: "404 Error", icon: "la la-exclamation-circle" },
+  { text: "500 Error", icon: "la la-exclamation-circle" },
+  { text: "Under Maintenance", icon: "la la-exclamation-circle" },
+  { text: "Pricing", icon: "la la-dollar" },
+  { text: "FAQs - Layout 1", icon: "la la-question-circle" },
+  { text: "FAQs - Layout 2", icon: "la la-question-circle" },
+  { text: "Invoice", icon: "la la-file-invoice-dollar" },
+];
 
 export const MenuDetailApplicationsItems = [
-  { text: "Media Library", icon: "la la-image"},
-  { text: "Point Of Sale", icon: "la la-shopping-bag"},
-  { text: "To Do", icon: "la la-check-circle"},
-  { text: "Chat", icon: "la la-comment"},
+  { text: "Media Library", icon: "la la-image" },
+  { text: "Point Of Sale", icon: "la la-shopping-bag" },
+  { text: "To Do", icon: "la la-check-circle" },
+  { text: "Chat", icon: "la la-comment" },
 ];
 
 export const MenuDetailMenuItems = [
@@ -60,4 +60,128 @@ export const MenuDetailMenuTypes = [
   { type: "hidden", icon: "la la-hand-point-left", label: "Hidden" },
   { type: "icon-only", icon: "la la-th-large", label: "Icon Only" },
   { type: "wide", icon: "la la-arrows-alt-h", label: "Wide" },
+];
+
+export const blogPosts = [
+  {
+    id: 1,
+    title: "Donec posuere metus vitae ipsum.",
+    overview:
+      "Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.",
+    details:
+      "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+
+    views: 702,
+    status: "Published",
+    createdDateTime: "1638982148",
+  },
+  {
+    id: 2,
+    title:
+      "Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.",
+    overview:
+      "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+    details:
+      "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.\n\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.",
+
+    views: 876,
+    status: "Draft",
+    createdDateTime: "1648017540",
+  },
+  {
+    id: 3,
+    title: "Etiam pretium iaculis justo.",
+    overview:
+      "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+    details:
+      "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
+
+    views: 362,
+    status: "Published",
+    createdDateTime: "1652146676",
+  },
+  {
+    id: 4,
+    title: "Phasellus in felis.",
+    overview:
+      "Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.",
+    details:
+      "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
+
+    views: 354,
+    status: "Draft",
+    createdDateTime: "1652727063",
+  },
+  {
+    id: 5,
+    title:
+      "Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue.",
+    overview:
+      "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.",
+    details:
+      "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.",
+    views: 554,
+    status: "Draft",
+    createdDateTime: "1630071683",
+  },
+  {
+    id: 6,
+    title: "Aliquam sit amet diam in magna bibendum imperdiet.",
+    overview:
+      "Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+    details:
+      "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+
+    views: 146,
+    status: "Published",
+    createdDateTime: "1637860648",
+  },
+  {
+    id: 7,
+    title: "Suspendisse accumsan tortor quis turpis.",
+    overview:
+      "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+    details:
+      "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+
+    views: 387,
+    status: "Draft",
+    createdDateTime: "1637103197",
+  },
+  {
+    id: 8,
+    title: "Vestibulum rutrum rutrum neque.",
+    overview:
+      "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.",
+    details:
+      "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+
+    views: 372,
+    status: "Draft",
+    createdDateTime: "1632136679",
+  },
+  {
+    id: 9,
+    title: "Nulla facilisi.",
+    overview:
+      "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.",
+    details:
+      "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+
+    views: 757,
+    status: "Published",
+    createdDateTime: "1658779178",
+  },
+  {
+    id: 10,
+    title: "Suspendisse potenti.",
+    overview:
+      "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+    details:
+      "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+
+    views: 711,
+    status: "Draft",
+    createdDateTime: "1635302785",
+  },
 ];
